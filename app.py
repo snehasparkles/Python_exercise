95a465feb1a6be20cafa64112ba5a2490f908110
snehasparkles <145270959+snehasparkles@users.noreply.github.com>
2024-02-03 14:44:31 +0000

    added changes

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for,session,abort
+from google_auth_oauthlib.flow import Flow
+import pathlib
+
+import os
+from pathlib import Path
 
 app = Flask('__name__')
 app.secret_key = 'secret key'
+GOOGLE_CLIENT_ID = "625444172244-4cnk1t6urql2mblfq359kdpj963sn8ui.apps.googleusercontent.com"
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+client_secret_file = os.path.join(Path(__file__).parent, 'client_secrete.json')
+
+
+flow = Flow.from_client_secrets_file(client_secrets_file=client_secret_file,scopes=['https://www.googleapis.com/auth/userinfo.profile','openid', 'https://www.googleapis.com/auth/userinfo.email'],redirect_uri='http://127.0.0.1:5000/callback')
 
 def login_required(function):
   def wrapper(*args, **kwargs):
@@ -16,14 +27,28 @@ def login_required(function):
 
 @app.route('/login')
 def login():
-  session['googe_id'] = "test"
-  return redirect("/protected")
+  authorization_url, state = flow.authorization_url()
+  session["state"] = state
+  return redirect(authorization_url)
   
 
 
 @app.route('/callback')
 def callback():
-  pass
+  flow.fetch_token(authorization_response=request.url)
+  if not session["state"] == request.args["state"]:
+    abort(500)
+  credentials = flow.credentials
+  request_session = requests.session()
+  cached_session = cachecontrol.CacheControl(request_session)
+  token_request = google.auth.transport.requests.Request(session=cached_session)
+
+  id_info = id_token.verify_oauth2_token(id_token=credentials._id_token,
+                                         request=token_request,
+                                         audience=GOOGLE_CLIENT_ID)
+  session["google_id"] = id_info.get("sub")
+  session["name"] = id_info.get("name")
+  return redirect('/protected')
 
 @app.route('/logout')
 def logout():
@@ -42,4 +67,4 @@ def protected():
   return "<h1> protected</h1> <a href='/logout' ><button>Logout</button></a> "
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080,debug=True)
+  app.run(debug=True)
